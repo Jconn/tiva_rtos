56ab415b417ec47be3a7728bb77049de74b20442
Jconn <johnconn@umich.edu>
2015-08-22 20:41:38 -0400

    Initial Commit for tiva tm4c123h6pmi microcontroller -TODO:configure systick exception for context switching

---
+++ ti_rtos_jc/Debug/sysCall.pp
@@ -0,0 +1,9 @@
+# FIXED
+
+sysCall.obj: ../sysCall.c
+sysCall.obj: ../sysCall.h
+sysCall.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.4/include/stdint.h
+
+../sysCall.c: 
+../sysCall.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.4/include/stdint.h: 
---
+++ ti_rtos_jc/sysCall.c
@@ -0,0 +1,15 @@
+/*
+ * sysCall.c
+ *
+ *  Created on: Aug 19, 2015
+ *      Author: John
+ */
+
+#include "sysCall.h"
+#include <stdint.h>
+
+
+void ThreadRequest(char * userStack)
+{
+	ProcessReturn();
+}
